41e3f373aa1096d3c354ff0a745c005e9db7917c
Ali Kedwaii <ali.kedwaii@utexas.edu>
2017-03-11 00:42:11 -0600

    updated individual critters and docs

--- src/assignment4/Critter1.java
+++ src/assignment4/Critter1.java
@@ -1,8 +1,7 @@
 package assignment4;
 
-/**
- * Created by Ali on 3/7/2017.
- */
+import java.util.List;
+
 public class Critter1 extends Critter {
 
 
@@ -10,7 +9,9 @@ public class Critter1 extends Critter {
         super();
     }
 
-
+    /**
+     * @return ASCII representation of critter
+     */
     public String toString() { return "A"; }
 
 
@@ -43,5 +44,22 @@ public class Critter1 extends Critter {
         reproduce(baby, Critter.getRandomInt(7));
 
         return true;
+    }
+
+    /**
+     * Displays total number of critters, along with average energy
+     * @param critters List of Critter1 objects in population
+     */
+    public static void runStats (List<Critter> critters) {
+        System.out.print("Total Critter1's: " + critters.size() + "\t");
+        if (critters.size() > 0) {
+            int avgNRG = 0;
+            for (Critter c : critters) {
+                avgNRG += ((Critter1)c).getEnergy();
+            }
+            avgNRG /= critters.size();
+            System.out.print("Avg Energy: " + avgNRG);
+        }
+        System.out.println();
     }
 }
